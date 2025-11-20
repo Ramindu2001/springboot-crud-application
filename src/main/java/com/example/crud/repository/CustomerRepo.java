f0d7ae532da97d87c12db8a9f9a0b13900a5c2ef
Ramindu2001 <ramindurantharu@gmail.com>
2025-11-20 11:30:41 +0530

    create customerRepo

---
+++ src/main/java/com/example/crud/repository/CustomerRepo.java
@@ -0,0 +1,10 @@
+package com.example.crud.repository;
+
+import com.example.crud.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepo extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
+}
